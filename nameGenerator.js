8663d2753a567961c899fbb88a0aba295be8cb69
hector27gab <147425846+hector27gab@users.noreply.github.com>
2023-11-16 04:17:28 +0000

    generador-de-nombres-de-dominio-by-hg

---
+++ nameGenerator.js
@@ -0,0 +1,14 @@
+let domain = () => {
+  let pronouns = ["/the/", "/our", "/IDK"];
+  let adjs = ["/great/", "big", "/npi"];
+  let nouns = ["/jogger/.", "racoon", "BTW"];
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        console.log("https//" + pronoun + "" + adj + "" + noun + ".com");
+      }
+    }
+  }
+};
+domain();
